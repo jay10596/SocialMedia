17abbe6d4e05b08c88983b3fa54578934d12ce15
Jay Modi <jay.modi105@gmail.com>
2022-02-01 21:55:05 +1000

    Initial Commit

--- src/migrations/2_deploy_contracts.js
+++ src/migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
-const Marketplace = artifacts.require("Marketplace");
+const SocialMedia = artifacts.require("SocialMedia");
 
 module.exports = function (deployer) {
-    deployer.deploy(Marketplace);
+    deployer.deploy(SocialMedia);
 };
